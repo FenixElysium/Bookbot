e0a097dc627d1b083617aa61cfdd4f0bae03871c
FenixElysium <uyrobinhood@gmail.com>
2025-12-13 22:55:14 -0600

    separated word counter in main.py into stats.py

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
+from stats import get_num_words
+
+
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
 
-def count_words(text):
-    words = text.split()
-    return len(words)
-
-
 def main():
     book_text = get_book_text("books/frankenstein.txt")
-    num_words = count_words(book_text)
+    num_words = get_num_words(book_text)
     print(f"Found {num_words} total words")
 
 
 main()
+
