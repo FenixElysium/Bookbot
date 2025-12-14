3276d0e4992d152b79e01825c0ffd81e3d3f28fb
FenixElysium <uyrobinhood@gmail.com>
2025-12-13 22:48:25 -0600

    modified main.py with proper coding

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as f:
+        return f.read()
+
+
+def main():
+    book_text = get_book_text("books/frankenstein.txt")
+    print(book_text)
+
+
+main()
