398edd588efe00bbab9e6591e57e86f81287bceb
FenixElysium <uyrobinhood@gmail.com>
2025-12-14 00:04:21 -0600

    added sys.argv to main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_num_words, count_characters, get_sorted_characters
 
 
@@ -7,17 +8,26 @@ def get_book_text(filepath):
 
 
 def main():
-    book_path = "books/frankenstein.txt"
+    # Check for command-line arguments
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
+
+    # Read the book
     text = get_book_text(book_path)
 
+    # Word count
     num_words = get_num_words(text)
-    char_counts = count_characters(text)
-    sorted_chars = get_sorted_characters(char_counts)
-
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
+
+    # Character count
+    char_counts = count_characters(text)
+    sorted_chars = get_sorted_characters(char_counts)
     print("--------- Character Count -------")
 
     for item in sorted_chars:
@@ -27,3 +37,4 @@ def main():
 
 
 main()
+
